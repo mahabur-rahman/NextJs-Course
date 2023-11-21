60950d598bd5845c1619620eaacbb87ebbcc444c
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-11-21 19:37:31 +0600

    prisma with mongodb

--- app/api/posts/page.js
+++ app/api/posts/page.js
@@ -1,5 +1,5 @@
-import AddPost from "./components/AddPost";
-import PostList from "./components/PostList";
+import AddPost from "../components/AddPost";
+import PostList from "../components/PostList";
 
 async function getData() {
     const res = await fetch("http://localhost:3000/api/posts", { cache: 'no-store' });
